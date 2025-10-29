bdb480b0f85217906172b078589d3379ee568c1a
Joe Butler <130206285+joe-butler-23@users.noreply.github.com>
2025-10-29 13:22:39 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,18 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Wayland portals so file pickers, screenshare, xdg-open, etc. work
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal.xdgOpenUsePortal = true;
+  
+  # Thunar needs these services for mounts and thumbnails
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+  
+  # Polkit (auth prompts). The agent will be launched by your Hyprland config.
+  security.polkit.enable = true;
+
   # Select internationalisation properties.
    i18n.defaultLocale = "en_GB.UTF-8";
   # console = {
@@ -75,10 +87,19 @@
      foot
      thunar
      mousepad
+     xdg-desktop-portal-
+     hyprland polkit_gnome
    ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+
+  # Helpful Wayland env tweaks for apps
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
