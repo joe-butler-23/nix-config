66f7a926d737aa53b66006764d040339f4b6fa36
Joe Butler <130206285+joe-butler-23@users.noreply.github.com>
2025-10-29 14:19:00 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-{
+={
   description = "My NixOS Config";
 
   inputs = {
@@ -6,10 +6,18 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    pkgsUnstable = import nixpkgs-unstable { inherit system; };
+  in
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+      specialArgs = {
+        inherit pkgsUnstable;
+      };
       modules = [ ./configuration.nix ];
     };
   };
