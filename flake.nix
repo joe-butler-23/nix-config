79ecefc3da88debd6a65acce7a6f597cbbc5f4d2
Joe Butler <130206285+joe-butler-23@users.noreply.github.com>
2025-10-29 14:17:53 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "My NixOS Config";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = inputs@{ self, nixpkgs, ...}: {
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
