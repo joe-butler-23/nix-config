fd97e49431ebbcb0a9e9c3875bde46461a9ffd3b
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 13:56:21 +0000

    Fix mako configuration - use services.mako.settings
    - Move all mako options under services.mako.settings
    - Update deprecated option names to new format (e.g., defaultTimeout -> default-timeout)
    - Eliminate evaluation warnings

--- modules/home/dotfiles/mako.nix
+++ modules/home/dotfiles/mako.nix
@@ -2,20 +2,22 @@ _: {
   services.mako = {
     enable = true;
 
-    font = "JetBrainsMono Nerd Font 11";
-    backgroundColor = "#1e1e2e";
-    textColor = "#cdd6f4";
-    borderColor = "#585b70";
-    borderSize = 2;
-    borderRadius = 8;
-    width = 350;
-    height = 120;
-    maxVisible = 5;
-    defaultTimeout = 5000;
-    margin = "10,10,0,0";
-    padding = "8,10";
-    progressColor = "#89b4fa";
-    layer = "overlay";
-    anchor = "top-right";
+    settings = {
+      font = "JetBrainsMono Nerd Font 11";
+      background-color = "#1e1e2e";
+      text-color = "#cdd6f4";
+      border-color = "#585b70";
+      border-size = 2;
+      border-radius = 8;
+      width = 350;
+      height = 120;
+      max-visible = 5;
+      default-timeout = 5000;
+      margin = "10,10,0,0";
+      padding = "8,10";
+      progress-color = "#89b4fa";
+      layer = "overlay";
+      anchor = "top-right";
+    };
   };
 }
