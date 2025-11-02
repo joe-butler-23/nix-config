7c55080c5c6a9673108bbf77fe8e97481a4dc566
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 14:22:16 +0000

    Simplify Syncthing configuration
    - Remove environment variable approach for now
    - Keep basic declarative setup with tray icon
    - Configure via web UI at http://localhost:8384

--- modules/home/services.nix
+++ modules/home/services.nix
@@ -1,28 +1,11 @@
 # modules/home/services.nix
 _: {
-  #### Syncthing - file synchronization (private config via env vars)
+  #### Syncthing - file synchronization
   services.syncthing = {
     enable = true;
     tray.enable = true;
 
-    # Use environment variables for private configuration
-    # Set these in ~/.config/syncthing-env before running home-manager switch
-    # SYNCTHING_DEVICE_ID=your-device-id
-    # SYNCTHING_DEVICES={"device1":{"id":"ID1"},"device2":{"id":"ID2"}}
-    # SYNCTHING_FOLDERS={"folder1":{"path":"~/folder1","devices":["device1"]}}
-
-    # Parse JSON from environment variables if set, otherwise empty
-    devices =
-      if builtins.getEnv "SYNCTHING_DEVICES" != ""
-      then builtins.fromJSON (builtins.getEnv "SYNCTHING_DEVICES")
-      else {};
-
-    folders =
-      if builtins.getEnv "SYNCTHING_FOLDERS" != ""
-      then builtins.fromJSON (builtins.getEnv "SYNCTHING_FOLDERS")
-      else {};
-
-    # Basic settings
+    # Basic settings - configure devices/folders via web UI at http://localhost:8384
     extraOptions = {
       gui = {
         theme = "dark";
