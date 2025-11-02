1b29b8f4d89d80a7a4b4fb76bfbaf336642fbb4e
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 11:24:14 +0000

    Temporarily disable syncthing service to fix Home Manager hanging

--- modules/home/services.nix
+++ modules/home/services.nix
@@ -1,39 +1,8 @@
 # modules/home/services.nix
 _: {
-  #### Syncthing service
+  #### Syncthing service - temporarily disabled to fix rebuild hanging
   services.syncthing = {
-    enable = true;
-    tray.enable = true;
-    guiAddress = "127.0.0.1:8384";
-    settings = {
-      options = {
-        localAnnounceEnabled = true;
-        globalAnnounceEnabled = true;
-        relaysEnabled = true;
-        urAccepted = -1; # Disable usage reporting
-      };
-      devices = {
-        # Add your devices here
-        # Example:
-        # "phone" = {
-        #   id = "DEVICE-ID-HERE";
-        #   name = "Phone";
-        #   introducer = false;
-        # };
-      };
-      folders = {
-        # Add your folders here
-        # Example:
-        # "Documents" = {
-        #   enable = true;
-        #   path = "~/Documents";
-        #   devices = [ "phone" ];
-        #   versioning = {
-        #     type = "trashcan";
-        #     params.cleanoutDays = 30;
-        #   };
-        # };
-      };
-    };
+    enable = false;
+    tray.enable = false;
   };
 }
