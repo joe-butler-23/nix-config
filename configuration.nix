05e5fe845c800b28ef68ad4be6b611dbe6c595d7
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 16:58:23 +0000

    Fix Stylix theming after restart
    - Moved Stylix from NixOS to Home Manager due to breaking change
    - Removed Stylix import from configuration.nix
    - Added Stylix Home Manager module and config to home.nix
    - This ensures Stylix activation works properly on login
    - Fixes Waybar theming and wallpaper persistence issues

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
     ./hardware-configuration.nix
     ./modules/sys/packages.nix
     ./modules/sys/services.nix
-    ./modules/sys/stylix.nix
+    # ./modules/sys/stylix.nix  # Moved to home.nix due to Stylix breaking change
   ];
 
   #### Boot configuration
