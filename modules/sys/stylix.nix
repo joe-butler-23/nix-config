2ace0af644e9250c2470159026a2f95275fb961b
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 17:27:13 +0000

    Revert to Nord Light color scheme
    - Changed back from custom Nord colors to base16 Nord Light
    - Restores the original light theme for GTK applications
    - Manual themes (Foot, Yazi, etc.) remain unchanged

--- modules/sys/stylix.nix
+++ modules/sys/stylix.nix
@@ -2,25 +2,8 @@
   stylix = {
     enable = true;
 
-    # Custom Nord color palette
-    base16Scheme = {
-      base00 = "1d2129"; # background
-      base01 = "292e39"; # regular0 (black)
-      base02 = "4c566a"; # bright0 (bright black)
-      base03 = "434c5e";
-      base04 = "d8dee9"; # foreground
-      base05 = "e5e9f0"; # regular7 (white)
-      base06 = "eceff4"; # bright7 (bright white)
-      base07 = "8fbcbb"; # bright6 (bright cyan)
-      base08 = "bf616a"; # regular1 (red)
-      base09 = "d08770";
-      base0A = "ebcb8b"; # regular3 (yellow)
-      base0B = "a3be8c"; # regular2 (green)
-      base0C = "88c0d0"; # regular6 (cyan)
-      base0D = "81a1c1"; # regular4 (blue)
-      base0E = "b48ead"; # regular5 (magenta)
-      base0F = "5e81ac";
-    };
+    # Nord Light color scheme
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord-light.yaml";
 
     # Wallpaper (relative path for flake purity)
     image = ./../../wallpaper.jpeg;
