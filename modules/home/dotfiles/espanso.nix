710bc5c694710cfdec1003042ee794dae24ae377
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-02 13:46:28 +0000

    Fix espanso - use manual config files instead of service
    - Remove services.espanso to avoid package collision
    - Use xdg.configFile with yaml.generate to create config files manually
    - Package already installed system-wide as espanso-wayland

--- modules/home/dotfiles/espanso.nix
+++ modules/home/dotfiles/espanso.nix
@@ -1,64 +1,57 @@
-{
-  pkgs,
-  ...
-}: {
-  services.espanso = {
-    enable = true;
-    package = pkgs.espanso-wayland;
-
-    configs = {
-      default = {
-        backend = "auto";
-        injector = "uinput";
-        keyboard_layout = {
-          layout = "gb";
-        };
+{config, ...}: let
+  yaml = config.lib.formats.yaml {};
+in {
+  # Espanso config files - package installed system-wide
+  xdg.configFile = {
+    "espanso/config/default.yml".source = yaml.generate "default.yml" {
+      backend = "auto";
+      injector = "uinput";
+      keyboard_layout = {
+        layout = "gb";
       };
     };
 
-    matches = {
-      base = {
-        matches = [
-          {
-            trigger = ":espanso";
-            replace = "Hi there!";
-          }
-          {
-            trigger = ":date";
-            replace = "{{mydate}}";
-            vars = [
-              {
-                name = "mydate";
-                type = "date";
-                params = {
-                  format = "%m/%d/%Y";
-                };
-              }
-            ];
-          }
-          {
-            trigger = ":shell";
-            replace = "{{output}}";
-            vars = [
-              {
-                name = "output";
-                type = "shell";
-                params = {
-                  cmd = "echo 'Hello from your shell'";
-                };
-              }
-            ];
-          }
-          {
-            trigger = ":email";
-            replace = "joeb.92@hotmail.co.uk";
-          }
-          {
-            trigger = ":gmail";
-            replace = "your-email@gmail.com";
-          }
-        ];
-      };
+    "espanso/match/base.yml".source = yaml.generate "base.yml" {
+      matches = [
+        {
+          trigger = ":espanso";
+          replace = "Hi there!";
+        }
+        {
+          trigger = ":date";
+          replace = "{{mydate}}";
+          vars = [
+            {
+              name = "mydate";
+              type = "date";
+              params = {
+                format = "%m/%d/%Y";
+              };
+            }
+          ];
+        }
+        {
+          trigger = ":shell";
+          replace = "{{output}}";
+          vars = [
+            {
+              name = "output";
+              type = "shell";
+              params = {
+                cmd = "echo 'Hello from your shell'";
+              };
+            }
+          ];
+        }
+        {
+          trigger = ":email";
+          replace = "joeb.92@hotmail.co.uk";
+        }
+        {
+          trigger = ":gmail";
+          replace = "your-email@gmail.com";
+        }
+      ];
     };
   };
 }
