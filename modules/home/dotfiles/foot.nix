4e3ca34b1277453486032635194a69fe4a3296b6
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-11-03 09:06:29 +0000

    Update Foot terminal colors to Nord Light theme
    - Changed foreground to 2E3440 and background to D8DEE9
    - Updated regular colors to match Nord Light palette
    - Preserved bright and dim color schemes
    - Maintains consistency with system-wide Nord Light theme

--- modules/home/dotfiles/foot.nix
+++ modules/home/dotfiles/foot.nix
@@ -15,18 +15,18 @@ _: {
         style = "beam";
       };
 
-      # Nord dark theme - overrides Stylix theming
+      # Nord Light theme - overrides Stylix theming
       colors = {
-        foreground = "d8dee9";
-        background = "2e3440";
+        foreground = "2E3440";
+        background = "D8DEE9";
 
-        regular0 = "3b4252"; # black
-        regular1 = "bf616a"; # red
-        regular2 = "a3be8c"; # green
-        regular3 = "ebcb8b"; # yellow
-        regular4 = "81a1c1"; # blue
-        regular5 = "b48ead"; # magenta
-        regular6 = "88c0d0"; # cyan
+        regular0 = "3B4252"; # black
+        regular1 = "6b363b"; # red
+        regular2 = "4f673a"; # green
+        regular3 = "968259"; # yellow
+        regular4 = "334d67"; # blue
+        regular5 = "5c3d57"; # magenta
+        regular6 = "3d6665"; # cyan
         regular7 = "e5e9f0"; # white
 
         bright0 = "4c566a"; # bright black
@@ -37,15 +37,6 @@ _: {
         bright5 = "b48ead"; # bright magenta
         bright6 = "8fbcbb"; # bright cyan
         bright7 = "eceff4"; # bright white
-
-        dim0 = "373e4d"; # dim black
-        dim1 = "94545d"; # dim red
-        dim2 = "809575"; # dim green
-        dim3 = "b29e75"; # dim yellow
-        dim4 = "68809a"; # dim blue
-        dim5 = "8c738c"; # dim magenta
-        dim6 = "6d96a5"; # dim cyan
-        dim7 = "aeb3bb"; # dim white
       };
     };
   };
