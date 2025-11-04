595a121b44cf570f2c121a8b2a51ce2f123a7c81
Joe Butler <youremail@example.com>
2025-11-04 12:45:14 +0000

    fix host module syntax

--- modules/hosts/laptop-nix.nix
+++ modules/hosts/laptop-nix.nix
@@ -1,5 +1,9 @@
-{ ... }:
-  
-  #### Host name
+{ lib, ... }:
+{
+  # Host name
   networking.hostName = "laptop-nix";
-  };
+
+  # If you want these here (optional; shared config can hold them instead):
+  # networking.networkmanager.enable = true;
+  # networking.firewall.enable = true;
+}
