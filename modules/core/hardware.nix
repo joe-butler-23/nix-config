c318d60394784cb29eff9ac26ff9615698128b9e
Joe Butler <youremail@example.com>
2025-11-26 18:57:04 +0000

    refactor: apply deadnix and statix suggestions to overlays and hardware

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -8,46 +8,50 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["dm-snapshot" "cryptd"];
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-label/NIXOS_LUKS";
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "btrfs";
-    options = ["subvol=root" "compress=zstd" "noatime"];
+  boot = {
+    initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
+    initrd.kernelModules = ["dm-snapshot" "cryptd"];
+    initrd.luks.devices."cryptroot".device = "/dev/disk/by-label/NIXOS_LUKS";
+    kernelModules = ["kvm-amd"];
+    extraModulePackages = [];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXOS_BOOT";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_ROOT";
+      fsType = "btrfs";
+      options = ["subvol=root" "compress=zstd" "noatime"];
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "btrfs";
-    options = ["subvol=home" "compress=zstd" "noatime"];
-  };
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_BOOT";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
-  };
+    "/home" = {
+      device = "/dev/disk/by-label/NIXOS_ROOT";
+      fsType = "btrfs";
+      options = ["subvol=home" "compress=zstd" "noatime"];
+    };
 
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd" "noatime"];
-  };
+    "/nix" = {
+      device = "/dev/disk/by-label/NIXOS_ROOT";
+      fsType = "btrfs";
+      options = ["subvol=nix" "compress=zstd" "noatime"];
+    };
 
-  fileSystems."/var/log" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "btrfs";
-    options = ["subvol=log" "compress=zstd" "noatime"];
+    "/persist" = {
+      device = "/dev/disk/by-label/NIXOS_ROOT";
+      fsType = "btrfs";
+      options = ["subvol=persist" "compress=zstd" "noatime"];
+    };
+
+    "/var/log" = {
+      device = "/dev/disk/by-label/NIXOS_ROOT";
+      fsType = "btrfs";
+      options = ["subvol=log" "compress=zstd" "noatime"];
+    };
   };
 
   swapDevices = [];
