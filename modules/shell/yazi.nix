93b84e2a0131bccd50ad3404c8ef6aba36999334
Joe Butler <youremail@example.com>
2025-12-18 13:21:50 +0000

    feat(yazi): add 'cy' binding to copy file contents

--- modules/shell/yazi.nix
+++ modules/shell/yazi.nix
@@ -37,6 +37,11 @@
     keymap = {
       mgr.prepend_keymap = [
         {
+          on = ["c" "y"];
+          run = ''shell 'wl-copy < "$1" && notify-send "Yazi" "Copied contents of $1"' --confirm'';
+          desc = "Copy file contents";
+        }
+        {
           on = "z";
           run = ''shell 'dest=$(fd . ~/downloads ~/documents ~/projects ~/development ~/orgflow ~/utilities ~/nix-config --type d --max-depth 2 | fzf) && [ -n "$dest" ] && ya emit cd "$dest"' --block'';
           desc = "Jump to directory (restricted)";
