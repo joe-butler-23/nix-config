e753affdf5ae112148c09c70521b627ed7652ed1
Joe Butler <youremail@example.com>
2025-12-21 20:36:04 +0000

    chore: Update emacs.d files to home-manager symlinks
    Emacs config files are now managed as home-manager symlinks
    to the nix store, ensuring atomic updates with system rebuilds.

--- modules/editor/emacs.d/post-early-init.el
+++ modules/editor/emacs.d/post-early-init.el
@@ -1,9 +1,1 @@
-;;; post-early-init.el --- Post-early-init configuration -*- lexical-binding: t; -*-
-
-;; Add padding/margins to the Emacs frame
-;; internal-border-width adds space between the frame border and the text area (top/bottom/left/right)
-(add-to-list 'default-frame-alist '(internal-border-width . 12))
-
-;; Set fringe widths (left/right margins for indicators)
-(add-to-list 'default-frame-alist '(left-fringe . 12))
-(add-to-list 'default-frame-alist '(right-fringe . 12))
+/nix/store/9qdyh1058hc3dhmk37jkfgzavarf28i3-home-manager-files/.emacs.d/post-early-init.el
--- modules/editor/emacs.d/pre-early-init.el
+++ modules/editor/emacs.d/pre-early-init.el
@@ -1,4 +1,1 @@
-;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
-;; NOTE: This must be placed in 'pre-early-init.el'.
-(setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+/nix/store/9qdyh1058hc3dhmk37jkfgzavarf28i3-home-manager-files/.emacs.d/pre-early-init.el
