d3a2b6db84d40701753f091893c064c94e9b50a8
Joe Butler <youremail@example.com>
2025-12-23 16:17:32 +0000

    feat(secrets): configure TODOIST_TOKEN via sops

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -10,10 +10,16 @@
       mode = "0440";
       owner = user;
     };
+
+    secrets.TODOIST_TOKEN = {
+      mode = "0440";
+      owner = user;
+    };
   };
 
-  # Make CONTEXT7_API_KEY available as environment variable
+  # Make secrets available as environment variables
   environment.variables = {
     CONTEXT7_API_KEY_PATH = "/run/secrets/CONTEXT7_API_KEY";
+    TODOIST_TOKEN = "$(cat /run/secrets/TODOIST_TOKEN)";
   };
 }
